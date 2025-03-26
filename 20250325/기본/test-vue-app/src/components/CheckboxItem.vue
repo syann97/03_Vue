bce2d09ef842935b475deedf32ae7dc0eec0e4f3
syann97 <shallom75321@gmail.com>
2025-03-26 17:04:15 +0900

    20250325 기본 & 심화 문제

---
+++ 20250325/기본/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li><input type="checkbox" :checked="checked" />{{ name }}</li>
+</template>
+<script>
+export default {
+  name: 'CheckboxItem',
+  props: {
+    id: [Number, String],
+    name: String,
+    checked: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ 20250325/기본/test-vue-app/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+// import App from './App.vue';
+// import App from './App2.vue';
+import App from './App4.vue';
+createApp(App).mount('#app');
